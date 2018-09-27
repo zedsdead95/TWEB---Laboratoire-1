a6dc05402ca23c70e46899cc7c02e962121d7560
zedsdead <mr.walid.koubaa@gmail.com>
2018-09-27 17:39:43 +0200

    Rendu Final Laboratoire 1

---
+++ js/killshot.js
@@ -0,0 +1,13 @@
+function rip(){
+    let mgk = document.getElementById('slimshady');
+    mgk.src = "img/2.jpg";
+    let up = document.getElementById('text1');
+    up.innerText = "0";
+}
+
+function save(){
+  let shadysBack = document.getElementById('slimshady');
+  shadysBack.src = "img/1.jpg";
+  let up = document.getElementById('text1');
+  up.innerText = "400k";
+}
